0bf4b43d4bd68a882635b79db3e82e83d314959a
Dainom <kyivpig@gmail.com>
2021-08-12 08:05:35 +0300

    1. Определите тип данных для переменных. [#172634]

---
+++ src/ru/job4j/Primitive.java
@@ -0,0 +1,26 @@
+package ru.job4j;
+
+public class Primitive {
+    public static void main(String[] arg){
+
+        byte age = 22;
+        boolean male = true; //or String gender = "male";
+        float height = 1.80f;
+        short bricks = 1500;
+        int bacterium = 1800000;
+        int distanceToSpace = 450830000;
+        boolean workPermits = true;
+        float fileSize = 1.025f;
+
+        System.out.println(age);
+        System.out.println(male);
+        System.out.println(height);
+        System.out.println(bricks);
+        System.out.println(bacterium);
+        System.out.println(distanceToSpace);
+        System.out.println(workPermits);
+        System.out.println(fileSize);
+
+
+    }
+}
