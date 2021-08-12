1a7c407f6ed39da921a762c6461f22498b4ae915
Dainom <kyivpig@gmail.com>
2021-08-12 13:20:43 +0300

    6. Идеальный вес [#41584]

---
+++ src/ru/job4j/Fit.java
@@ -0,0 +1,25 @@
+package ru.job4j;
+
+public class Fit {
+
+    public static double manWeight(short heigth) {
+        return (heigth - 100) * 1.15;
+    }
+
+    public static double womanWeight(short heigth) {
+        return (heigth - 110) * 1.15;
+    }
+
+    public static void main(String[] args) {
+
+        // Man
+        short manHeight = 183;
+        double man = Fit.manWeight(manHeight);
+        System.out.println("Man 183 is " + man);
+
+        // Woman
+        short womanHeight = 170;
+        double woman = Fit.womanWeight(womanHeight);
+        System.out.println("Woman 170 is " + woman);
+    }
+}
