34423b90a824d4e4f24a548a261cdd35ca35bf53
Dainom <kyivpig@gmail.com>
2021-08-13 08:28:12 +0300

    4. Методы [#231984 #188982]

--- src/ru/job4j/Args.java
+++ src/ru/job4j/Args.java
@@ -1,25 +1,15 @@
 package ru.job4j;
 
 public class Args {
-    public static void plus(
-            int hundred,
-            int fiveHundred,
-            int four,
-            int two,
-            int three,
-            int five
-    ) {
 
-        int result1 = hundred + fiveHundred;
-        int result2 = four + two;
-        int result3 = three + five;
-
-        System.out.println(result1);
-        System.out.println(result2);
-        System.out.println(result3);
+    public static void plus(int first, int second) {
+        int result = first + second;
+        System.out.println(result);
     }
 
     public static void main(String[] args) {
-        Args.plus(100, 500, 4, 2, 3, 5);
+        Args.plus(100, 500);
+        Args.plus(1, 2);
+        Args.plus(3, 5);
     }
 }
