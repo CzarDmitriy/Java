9ae9463781831c26e920fa47c3450681754e15da
Dainom <kyivpig@gmail.com>
2021-08-13 12:23:04 +0300

    9.1. String [#237407]

---
+++ src/ru/job4j/Greeting.java
@@ -0,0 +1,9 @@
+package ru.job4j;
+
+public class Greeting {
+    public static void main(String[] args) {
+        String idea = "I like Java!";
+        int year = 2021;
+        System.out.println(idea + year + " But I am a newbie");
+    }
+}
