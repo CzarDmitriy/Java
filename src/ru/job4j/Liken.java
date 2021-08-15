ccb372723c09843e8f3129df1aa4d76d23d85982
Dainom <kyivpig@gmail.com>
2021-08-15 09:42:32 +0300

    0. Операции сравнения [#235631]

---
+++ src/ru/job4j/Liken.java
@@ -0,0 +1,17 @@
+package ru.job4j;
+
+public class Liken {
+    public static void main(String[] args) {
+        int first = 10;
+        int second = 9;
+
+        boolean result = first > second; //true
+        System.out.println(result);
+
+        boolean result2 = first < second; // false
+        System.out.println(result2);
+
+        boolean result3 = first == second; // false
+        System.out.println(result3);
+    }
+}
