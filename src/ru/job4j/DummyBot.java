e3fb041f5a09a44fef3d2b063f0a25cf8974e01c
Dainom <kyivpig@gmail.com>
2021-08-15 13:23:32 +0300

    4. Тесты для стороны прямоугольника. [#159460 #189958]

---
+++ src/ru/job4j/DummyBot.java
@@ -0,0 +1,32 @@
+package ru.job4j;
+
+public class DummyBot {
+
+    public static String answer(String question) {
+
+        String rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+
+        if ("Привет, Бот.".equals(question)) {
+            rsl = "Привет, умник.";
+        }
+
+        if ("Пока.".equals(question)) {
+            rsl = "До скорой встречи.";
+        }
+
+        if ("Сколько будет 2 + 2".equals(question)) {
+            rsl = "Это ставит меня в тупик. Задайте другой вопрос.";
+        }
+
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+
+        String rsl = DummyBot.answer("Привет, Бот.");
+        System.out.println(rsl);
+
+        rsl = DummyBot.answer("Пока.");
+        System.out.println(rsl);
+    }
+}
