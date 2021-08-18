f8ea972cc7822a3ad8eea7a8d323be1497f8e66a
Dainom <kyivpig@gmail.com>
2021-08-18 12:19:47 +0300

    4.2. Тернарное сравнение [#189 #190098]

--- test/ru/job4j/MaxTest.java
+++ test/ru/job4j/MaxTest.java
@@ -6,11 +6,29 @@ import org.junit.Test;
 public class MaxTest {
 
     @Test
-    public void whenMax128to400Then400() {
-        int in1 = 128;
-        int in2 = 400;
+    public void whenMax128more400Then400() {
+        int left = 128;
+        int right = 400;
         int expected = 400;
-        int result = Max.max(in1, in2);
+        int result = left > right ? left : right;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax128less400Then128() {
+        int left = 128;
+        int right = 400;
+        int expected = 128;
+        int result = left < right ? left : right;
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void whenMax128equals400Then128() {
+        int left = 128;
+        int right = 400;
+        int expected = 400;
+        int result = left == right ? left : right;
         Assert.assertEquals(expected, result);
     }
 
