cb7f70acb3a2c8144a36b0338aafa2d54760dc16
Dainom <kyivpig@gmail.com>
2021-08-19 12:59:13 +0300

    4.3.6. Math.abs [#373616 #191416]]

--- src/ru/job4j/ChessBoard.java
+++ src/ru/job4j/ChessBoard.java
@@ -5,9 +5,13 @@ public class ChessBoard {
 
         int rsl = 0;
 
-        if (x1 >= 0 && x1 <= 0 && x2 >= 0 && x2 <= 0 && y1 >= 0 && y1 <= 0 && y2 >= 0 && y2 <= 0) {
+        if (x1 >= 0 && x1 <= 7 && x2 >= 0 && x2 <= 7 && y1 >= 0 && y1 <= 7 && y2 >= 0 && y2 <= 7) {
             rsl = Math.abs(y1 - x1);
             rsl = rsl == 0 ? Math.abs(y2 - y1) : rsl;
+
+            if (Math.abs(x2 - x1) == Math.abs(y2 - y1)) {
+                rsl = Math.abs(x2 - x1);
+            }
         }
 
         return rsl;
