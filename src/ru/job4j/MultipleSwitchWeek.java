cae64302e6caa0aa2e12e65d75e03bf9f01db7fd
Dainom <kyivpig@gmail.com>
2021-08-20 11:46:32 +0300

    4.6.2. Switch - matcher [#334071 #192136]

--- src/ru/job4j/MultipleSwitchWeek.java
+++ src/ru/job4j/MultipleSwitchWeek.java
@@ -4,10 +4,6 @@ public class MultipleSwitchWeek {
     public static int numberOfDay(String name) {
         return switch (name) {
             case "Понедельник", "Monday" -> 1;
-            case "Вторник", "Tuesday" -> 2;
-            case "Среда", "Wednesday" -> 3;
-            case "Четверг", "Thursday" -> 4;
-            /* continue? or is it wrong?*/
             default -> 0;
         };
     }
