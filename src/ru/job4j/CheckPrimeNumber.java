10f45785e5a60196993d79a982ce07be7882bcd9
Dainom <kyivpig@gmail.com>
2021-08-22 11:12:03 +0300

    5.2. Создать программу, вычисляющую факториал. [#193 #192519]

--- src/ru/job4j/CheckPrimeNumber.java
+++ src/ru/job4j/CheckPrimeNumber.java
@@ -2,14 +2,9 @@ package ru.job4j;
 
 public class CheckPrimeNumber {
     public static boolean check(int number) {
-        boolean prime = true;
-        for (int i = 0; i <= number; i++) {
-            if ((number % 5) == 0) {
-                break;
-            } else if ((number % 4) == 0) {
-                prime = false;
-                break;
-            } else if ((number % 1) == 0) {
+        boolean prime = number > 1;
+        for (int i = 2; i < number; i++) {
+            if ((number % i) == 0) {
                 prime = false;
                 break;
             }
