d9354837f7d4ce24099d69e7735f1f05a5c558d3
Dainom <kyivpig@gmail.com>
2021-08-23 09:17:49 +0300

    6.0.1.1. Размер массива [#333582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -2,9 +2,14 @@ package ru.job4j.array;
 
 public class ArrayDefinition {
     public static void main(String[] args) {
+
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
+
+        System.out.println("Массив ages равен: " + ages.length);
+        System.out.println("Массив surname равен: " + surnames.length);
+        System.out.println("Массив prices равен: " + prices.length);
 
     }
 }
