d181e497f6f1e135d7adc0a13de07ce55a6ec73b
Dainom <kyivpig@gmail.com>
2021-08-26 10:37:16 +0300

    6.5. Поиск индекса в диапазоне. [#156318]

---
+++ src/ru/job4j/array/FindLop.java
@@ -0,0 +1,43 @@
+package ru.job4j.array;
+
+public class FindLop {
+    public static int indexOf(int[] data, int el) {
+        int rst = -1;
+
+//          F A I L E D
+//        for (int i = 0; i < data.length; i++) {
+//            if (data[i] == el) {
+//                rst = el;
+//            } else {
+//                rst = -1;
+//            }
+//        }
+
+//        F A I L E D
+//        for (int i = 0; i < data.length; i++) {
+//            if (data[i] == el) {
+//                rst = el;
+//            }
+//        }
+
+//        F A I L E D
+//        for (int i = 0; i < data.length; i++) {
+//            if (el == data[i]) {
+//                rst = data[i];
+//            }
+//        }
+
+//        F A I L E D
+//        for (int i = 0; i < data.length; i++) {
+//            if (data[i] == data[0]) {
+//                rst = el;
+//            }
+//        }
+        return rst;
+    }
+
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;
+        return rst;
+    }
+}
